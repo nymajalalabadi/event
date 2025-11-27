5154d3c3f3f3243a11d819afc3c3968a9ebdb887
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-27 08:52:11 -0600

    Used Icons In EventItem

--- component/events/event-items.tsx
+++ component/events/event-items.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
 import classes from '../../styles/event-item.module.css';
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import AddressIcon from "../icons/address-icon";
+import ArrowRightIcon from "../icons/arrow-right-icon";
 
 export default function EventItem(props: { id: string, title: string, image: string, date: string, location: string, description: string }) {
 
@@ -23,14 +25,22 @@ export default function EventItem(props: { id: string, title: string, image: str
     <div className={classes.summary}>
       <h2>{title}</h2>
       <div className={classes.date}>
+        <DateIcon />
         <time>{formattedDate}</time>
       </div>
       <div className={classes.address}>
+        <AddressIcon />
         <address>{formattedAddress}</address>
       </div>
     </div>
     <div className={classes.actions}>
-      <Button href={exploreLink}>Explore Event</Button>
+      
+      <Button href={exploreLink}>
+        <span>Explore Event</span>
+        <span className={classes.icon}>
+          <ArrowRightIcon />
+        </span>
+        </Button>
     </div>
     </div>
   </li>
