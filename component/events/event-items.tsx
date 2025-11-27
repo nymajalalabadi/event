19fafa90de8d529753540e5f92dd18d6017cdca7
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-27 08:07:04 -0600

    Updated EventItem

--- component/events/event-items.tsx
+++ component/events/event-items.tsx
@@ -1,13 +1,36 @@
+import Link from "next/link";
 
 export default function EventItem(props) {
+
   const { id, title, image, date, location, description } = props;
+
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
+  const formattedAddress = location.replace(', ', '\n');
+
+  const exploreLink = `/events/${id}`;
+
   return (
   <li key={id}>
-    <img src={image} alt={title} />
+    <img src={'/' + image} alt={title} />
+    <div>
     <h2>{title}</h2>
-    <time>{date}</time>
-    <address>{location}</address>
-    <p>{description}</p>
+    <div>
+      <time>{formattedDate}</time>
+    </div>
+    <div>
+      <address>{formattedAddress}</address>
+    </div>
+    <div>
+      <Link href={exploreLink}>
+        <button>Explore Event</button>
+      </Link>
+    </div>
+    </div>
   </li>
   )
 }
