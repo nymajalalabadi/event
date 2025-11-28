b352e6702a765904fb211f5e7ea6b0c5dba39acf
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-11-28 08:37:51 -0600

    Added EventsSearch

--- component/ui/button.tsx
+++ component/ui/button.tsx
@@ -2,9 +2,18 @@ import classes from '../../styles/button.module.css';
 import Link from 'next/link';
 
 export default function Button(props: { children: React.ReactNode, href: string }) {
-  return (
-    <Link href={props.href} className={classes.btn}>
+
+  if (props.href) {
+    return (
+      <Link href={props.href} className={classes.btn}>
         {props.children}
-    </Link>
+      </Link>
+    )
+  }
+  
+    return (
+      <button className={classes.btn} onClick={props.onClick}>
+        {props.children}
+      </button>
   )
 }
