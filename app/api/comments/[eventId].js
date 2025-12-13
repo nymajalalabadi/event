44fb1a69b45f871b1f1823111500b82dc49a7efb
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-12-13 13:19:30 -0600

    Removed newsLetter.tsx file and updated tsconfig.json to include newsLetter.js for improved API structure.

---
+++ app/api/comments/[eventId].js
@@ -0,0 +1,27 @@
+export async function handler(req, res) {
+
+    const eventId = req.query.eventId;
+
+    if (req.method === 'POST') {
+        const { email, name, text } = req.body;
+
+        if (!email || email.trim() === '' || !name || name.trim() === '' || !text || text.trim() === '') {
+            return res.status(422).json({ message: 'Invalid input.' });
+        }
+
+        console.log(eventId, email, name, text);
+
+        return res.status(201).json({ message: 'Comment added!' });
+    }
+
+    if (req.method === 'GET') {
+        const dummyList = [
+            { id: 'c1', name: 'Max', text: 'A first comment!' },
+            { id: 'c2', name: 'Manuel', text: 'A second comment!' },
+        ];
+
+        return res.status(200).json({ comments: dummyList });
+    }
+
+    return new Response('Method not allowed', { status: 405 });
+}
